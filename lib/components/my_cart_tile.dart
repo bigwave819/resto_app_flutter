7199e163c0c2fd98868acc981f625e3bde6eefb4
Hirwa Tresor <waveb6133@gmail.com>
2025-07-25 19:55:05 +0200

    added more changes

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -13,45 +13,89 @@ class MyCartTile extends StatelessWidget {
     return Consumer<Restaurant>(
       builder:
           (context, restaurant, child) => Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(8)
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
             child: Column(
               children: [
-                Row(
-                  children: [
-                    //food image
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.asset(
-                        cartItem.food.imagePath,
-                        height: 100,
-                        width: 100,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      //food image
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset(
+                          cartItem.food.imagePath,
+                          height: 100,
+                          width: 100,
+                        ),
                       ),
-                    ),
-                    //name and price
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(cartItem.food.name),
-                        Text(cartItem.food.price.toString()),
-                      ],
-                    ),
+                      //name and price
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(cartItem.food.name),
+                          Text(cartItem.food.price.toString()),
+                        ],
+                      ),
 
-                    //increment or decrement quantity
-                    MyQuantitySelector(
-                      quantity: cartItem.quantity,
-                      food: cartItem.food,
-                      onIncrement: () {
-                        restaurant.removeFromCart(cartItem);
-                      },
-                      onDecrement: () {
-                        restaurant.addToCart(
-                          cartItem.food,
-                          cartItem.selectedAddons,
-                        );
-                      },
-                    ),
-                  ],
+                      const Spacer(),
+                  
+                      //increment or decrement quantity
+                      MyQuantitySelector(
+                        quantity: cartItem.quantity,
+                        food: cartItem.food,
+                        onIncrement: () {
+                          restaurant.removeFromCart(cartItem);
+                        },
+                        onDecrement: () {
+                          restaurant.addToCart(
+                            cartItem.food,
+                            cartItem.selectedAddons,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
                 //addons
+
+                SizedBox(
+                  height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                    children: cartItem.selectedAddons.map(
+                      (addon) => Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: FilterChip(
+                          label: Row(
+                            children: [
+                              //addon name
+                              Text(addon.name),
+                              //addon price
+                              Text(addon.price.toString()),
+                            ],
+                          ), 
+                          shape: StadiumBorder(
+                            side: BorderSide(
+                              color: Theme.of(context).colorScheme.primary
+                            )
+                          ),
+                          onSelected: (value) {},
+                          backgroundColor: Theme.of(context).colorScheme.secondary,
+                          labelStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                            fontSize: 20
+                          ),
+                        ),
+                      ),
+                    ).toList()
+                  ),
+                )
               ],
             ),
           ),
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:resto_app/components/my_button.dart';
 import 'package:resto_app/components/my_cart_tile.dart';
 import 'package:resto_app/models/restaurant.dart';
+import 'package:resto_app/pages/payment_page.dart';
 
 class Cartpage extends StatelessWidget {
   const Cartpage({super.key});
@@ -17,21 +19,78 @@ class Cartpage extends StatelessWidget {
             title: const Text("Cart"),
             backgroundColor: Colors.transparent,
             foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+            actions: [
+              //clear all cart
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder:
+                        (context) => AlertDialog(
+                          title: Text(
+                            "Are you sure you want to clear the cart?",
+                          ),
+                          actions: [
+                            //cancel button
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Cancel"),
+                            ),
+                            //yes button
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                restaurant.clearCart();
+                              },
+                              child: const Text("Cancel"),
+                            ),
+                          ],
+                        ),
+                  );
+                },
+                icon: const Icon(Icons.delete),
+              ),
+            ],
             elevation: 0,
           ),
           body: Column(
             children: [
               Expanded(
-                child: ListView.builder(
-                  itemCount: userCart.length,
-                  itemBuilder: (context, index) {
-                    //get individual cartItem
-                    final cartItem = userCart[index];
-                    //return cart tile UI
-                    return MyCartTile(cartItem: cartItem);
-                  },
+                child: Column(
+                  children: [
+                    // the cart items
+                    userCart.isEmpty
+                        ? const Expanded(
+                          child: Center(child: Text("The cart is Empty")),
+                        )
+                        : Expanded(
+                          child: ListView.builder(
+                            itemCount: userCart.length,
+                            itemBuilder: (context, index) {
+                              //get individual cartItem
+                              final cartItem = userCart[index];
+                              //return cart tile UI
+                              return MyCartTile(cartItem: cartItem);
+                            },
+                          ),
+                        ),
+                  ],
                 ),
               ),
+
+              //button to pay
+              MyButton(
+                text: "Go to the check out",
+                onTap:
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const PaymentPage(),
+                      ),
+                    ),
+              ),
+
+              const SizedBox(height: 35),
             ],
           ),
         );
